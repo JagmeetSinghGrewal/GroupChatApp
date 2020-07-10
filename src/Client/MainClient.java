9e6b44301e8859a775cae0252d6266223f650695
JagmeetSinghGrewal <jugee.grewal@gmail.com>
2020-07-10 14:31:47 +1000

    Added gitignore

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -1,5 +1,29 @@
 package Client;
 
-public class MainClient {
+import java.io.IOException;
+import java.net.Socket;
 
+public class MainClient {
+	
+	private static final String HOST = "localhost";
+	private static final int PORT = 9999;
+	private static final Boolean DEBUG = false;
+	
+	public static void main(String[] args) {
+		try {
+			Socket s = new Socket(HOST, PORT);
+		} catch (IOException e) {
+			print("Error in Socket: " + e.getLocalizedMessage());
+		}
+	}
+	
+	private static void print(String s) {
+		System.out.println(s);
+	}
+	
+	private static void debug(String s) {
+		if(DEBUG) {
+			System.out.println(s);
+		}
+	}
 }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,5 +1,34 @@
 package Server;
 
-public class MainServer {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class MainServer {
+	private final static int PORT = 9999;
+	private final static Boolean DEBUG = false;
+	
+	
+	public static void main(String[]  args) {
+		try {
+			ServerSocket s = new ServerSocket(PORT);
+			while(true) {
+				Socket so = s.accept();
+				print("A client has connected");
+			}
+		} catch (Exception e) {
+			print("Error in settings up server socket");
+			print(e.getLocalizedMessage());
+		}
+	}
+	
+	private static void print(String s) {
+		System.out.println(s);
+	}
+	
+	private static void debug(String s) {
+		if(DEBUG) {
+			System.out.println(s);
+		}
+	}
 }
+ 
